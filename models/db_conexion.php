f9813ecae0a793401ef7d56688b2e73af3b6b323
MARCO <mm_marco_mar@hotmail.com>
2022-02-10 11:21:04 -0600

    10/02/2022

--- models/db_conexion.php
+++ models/db_conexion.php
@@ -18,6 +18,21 @@ class ConexionsBd
 
         return $link;
     }
+    public static function conectar2()
+    {
+
+        $link =  new PDO(
+            "mysql:host=localhost;dbname=admon",
+            "root",
+            "",
+            array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+            )
+        );
+
+        return $link;
+    }
     public static function conectarParametros()
     {
 
